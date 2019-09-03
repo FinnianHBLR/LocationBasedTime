c1be6bc89f95163212c4da14a509e6a9120b4324
Finn <finnniansproject@gmail.com>
2019-09-03 19:31:09 +1200

    :bug: Changed time to 24hr

--- src/main/java/tech/teamio/lbt/TimeUtil.java
+++ src/main/java/tech/teamio/lbt/TimeUtil.java
@@ -8,7 +8,7 @@ public class TimeUtil {
     public static long calTime(){
     long time = 0;
 
-        DateFormat hours = new SimpleDateFormat("hh");
+        DateFormat hours = new SimpleDateFormat("HH");
         DateFormat minutes = new SimpleDateFormat("mm");
         Date date = new Date();
         String hoursStr = hours.format(date);
@@ -16,6 +16,7 @@ public class TimeUtil {
 
         double temp = (( Integer.parseInt(hoursStr)* 1000 ) - 6000 ) + ( Integer.parseInt(minutesStr)* 16.7 );
         time = (long)temp;
+        System.out.println(hoursStr + minutesStr + "=" + time);
         return time;
     }
 }
