52c506d1978818286017cb6fbe532298c395a1b8
Joe van der Zwet <me@joezwet.me>
2019-09-03 19:48:52 +1200

    :art: Remove unused imports.

--- src/main/java/tech/teamio/lbt/mixin/LevelPropertiesMixin.java
+++ src/main/java/tech/teamio/lbt/mixin/LevelPropertiesMixin.java
@@ -1,13 +1,9 @@
 package tech.teamio.lbt.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.world.World;
 import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tech.teamio.lbt.TimeUtil;
 
