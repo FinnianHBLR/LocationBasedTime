0c0e15534812d5af2fd7d00025a3d4cb253ecba4
Joe van der Zwet <me@joezwet.me>
2019-09-03 14:22:44 +1200

    Update and rename src/main/java/net/fabricmc/example/ExampleMod.java to src/main/java/tech/teamio/lbt/LocationBasedTime.java

---
+++ src/main/java/tech/teamio/lbt/LocationBasedTime.java
@@ -0,0 +1,10 @@
+package tech.teamio.lbt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LocationBasedTime implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// lol
+	}
+}
